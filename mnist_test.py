23110dc92772fc2b18ad5a38fe07853c48b05044
charliepapa2@gmail.com <charliepapa2@gmail.com>
2018-07-30 13:06:06 +0100

    dsfasdf

--- mnist_test.py
+++ mnist_test.py
@@ -1,46 +1,48 @@
 from keras.datasets import mnist
-from keras import models
-from keras import layers
-from keras.utils import to_categorical
 
-num_epochs = 5
-batch_size = 128
-
-image_width = 28
-image_height = 28
-
-# get the data set
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# build the network
-network = models.Sequential()
+from keras import models
+from keras import layers
 
-# dense means densely connected. In other words, all nodes are connected to all on next layer
-network.add(layers.Dense(512, activation='relu', input_shape=(image_width * image_height,)))
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
 network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
-
-# the data is 60k matrices. Reshape each image to a long array of (28*28) numbers
-train_images = train_images.reshape((60000, image_width * image_height))
-# gray-scale values go from 0-255. Change so they go from 0-1
+train_images = train_images.reshape((60000, 28*28))
 train_images = train_images.astype('float32') / 255
 
-# do same for test set
-test_images = test_images.reshape((10000, image_width * image_height))
+test_images = test_images.reshape((10000, 28*28))
 test_images = test_images.astype('float32') / 255
 
+from keras.utils import to_categorical
 
-# each element in these tensors is a number from 0-9. Change so they are 10-dim, 1-axis binary tensor
-# with only 1 on the value pertaining to the class. This is known as 1-hot encoding
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
-# train the network
-network.fit(train_images, train_labels, epochs=num_epochs, batch_size=batch_size)
+x_val = train_images[:1000]
+y_val = train_labels[:1000]
 
-# test the network
-test_loss, test_acc = network.evaluate(test_images, test_labels)
+train_images = train_images[1000:]
+train_labels = train_labels[1000:]
 
-print('test_acc:', test_acc)
+history = network.fit(train_images, train_labels, epochs=5, batch_size=128, validation_data=(x_val, y_val))
+
+
+
+import matplotlib.pyplot as plt
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
